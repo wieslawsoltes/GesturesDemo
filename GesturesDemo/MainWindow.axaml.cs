4a1ea8f68f397dbcc1c3bcf2d87af73691010c35
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-12-22 22:15:47 +0100

    Use normal panel

--- GesturesDemo/MainWindow.axaml.cs
+++ GesturesDemo/MainWindow.axaml.cs
@@ -12,20 +12,20 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
         
-        PanAndZoomTransformControl.AddHandler(PinchToZoomGestureRecognizer.PinchToZoomEvent, (s, e) =>
+        PanAndZoomPanel.AddHandler(PinchToZoomGestureRecognizer.PinchToZoomEvent, (s, e) =>
         {
             Console.WriteLine($"[PinchToZoom] GestureId='{e.GestureId}', Scale='{e.Scale}', Offset='{e.Offset}', Velocity='{e.Velocity}'");
             var scale = e.Scale;
             var offset = e.Offset;
             var matrix = Matrix.CreateScale(scale, scale) * Matrix.CreateTranslation(offset.X, offset.Y);
-            PanAndZoomTransformControl.LayoutTransform = new MatrixTransform(matrix);
+            PanAndZoomCanvas.RenderTransform = new MatrixTransform(matrix);
         });
 
-        PanAndZoomTransformControl.AddHandler(PanGestureRecognizer.PanGestureEvent, (s, e) =>
+        PanAndZoomPanel.AddHandler(PanGestureRecognizer.PanGestureEvent, (s, e) =>
         {
             Console.WriteLine($"[Pan] GestureId='{e.GestureId}', Delta='{e.Delta}', Velocity='{e.Velocity}'");
             var matrix = Matrix.CreateTranslation(e.Delta.X, e.Delta.Y);
-            PanAndZoomTransformControl.LayoutTransform = new MatrixTransform(matrix);
+            PanAndZoomCanvas.RenderTransform = new MatrixTransform(matrix);
         });
     }
 }
