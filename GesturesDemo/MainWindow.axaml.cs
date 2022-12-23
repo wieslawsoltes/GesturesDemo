ce7a7fb7cd1509ce2f008a2872f06e81a2bb5f57
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-12-23 21:37:09 +0100

    Update MainWindow.axaml.cs

--- GesturesDemo/MainWindow.axaml.cs
+++ GesturesDemo/MainWindow.axaml.cs
@@ -8,9 +8,18 @@ namespace GesturesDemo;
 
 public partial class MainWindow : Window
 {
+    private int _panGestureId = -1;
+    private int _pinchToZoomGestureId = -1;
+    private Matrix _startMatrix = Matrix.Identity;
+
     public MainWindow()
     {
         InitializeComponent();
+
+        if (PanAndZoomCanvas.RenderTransform is null)
+        {
+            PanAndZoomCanvas.RenderTransform = new MatrixTransform(Matrix.Identity);
+        }
         
         PanAndZoomPanel.AddHandler(PinchToZoomGestureRecognizer.PinchToZoomEvent, (s, e) =>
         {
@@ -18,14 +27,29 @@ public partial class MainWindow : Window
             var scale = e.Scale;
             var offset = e.Offset;
             var matrix = Matrix.CreateScale(scale, scale) * Matrix.CreateTranslation(offset.X, offset.Y);
-            PanAndZoomCanvas.RenderTransform = new MatrixTransform(matrix);
+
+            if (_pinchToZoomGestureId != e.GestureId)
+            {
+                _pinchToZoomGestureId = e.GestureId;
+                _startMatrix = PanAndZoomCanvas.RenderTransform.Value;
+            }
+
+            PanAndZoomCanvas.RenderTransform = new MatrixTransform(_startMatrix * matrix);
         });
 
         PanAndZoomPanel.AddHandler(PanGestureRecognizer.PanGestureEvent, (s, e) =>
         {
             Console.WriteLine($"[Pan] GestureId='{e.GestureId}', Delta='{e.Delta}', Velocity='{e.Velocity}'");
+
             var matrix = Matrix.CreateTranslation(e.Delta.X, e.Delta.Y);
-            PanAndZoomCanvas.RenderTransform = new MatrixTransform(matrix);
+
+            if (_panGestureId != e.GestureId)
+            {
+                _panGestureId = e.GestureId;
+                _startMatrix = PanAndZoomCanvas.RenderTransform.Value;
+            }
+
+            PanAndZoomCanvas.RenderTransform = new MatrixTransform(_startMatrix * matrix);
         });
     }
 }
